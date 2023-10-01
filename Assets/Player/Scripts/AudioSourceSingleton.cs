f3ed3c936ffdea578ec7a30b348c47fa93a642ec
AnahatM <anahat.mudgal@gmail.com>
2023-10-01 14:59:19 -0400

    audio source singleton

---
+++ Assets/Player/Scripts/AudioSourceSingleton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceSingleton : MonoBehaviour
+{
+    static AudioSourceSingleton instance;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
